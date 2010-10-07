7f90d0bf74f94a16e9c36c0a6bd5a8337784d722
Ren Yufei <renyufei83@yahoo.com.cn>
2010-10-06 22:50:01 -0400

    buf list debug

--- netkit-ftp-0.17/ftp/rdma.c
+++ netkit-ftp-0.17/ftp/rdma.c
@@ -643,7 +643,7 @@ int tsf_setup_buf_list(struct rdma_cb *cb)
 	int i;
 	BUFDATBLK *item;
 	
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 30; i++) {
 		if ( (item = (BUFDATBLK *) malloc(sizeof(BUFDATBLK))) == NULL) {
 			perror("tsf_setup_buf_list: malloc");
 			exit(EXIT_FAILURE);
@@ -909,10 +909,10 @@ recver(void *arg)
 	
 	for ( ; ; ) {
 	
-	/* wait for the client send ADV - READ? WRITE? */
+	/* wait for the client send ADV - READ? WRITE?
 	syslog(LOG_ERR, "before sem_wait(&cb->sem) 1 @ %x", (unsigned long) &cb->sem);
 	sem_wait(&cb->sem);
-	syslog(LOG_ERR, "after sem_wait(&cb->sem) 1");
+	syslog(LOG_ERR, "after sem_wait(&cb->sem) 1"); */
 	
 		/* get a free block */
 		TAILQ_LOCK(&free_tqh);
@@ -926,7 +926,7 @@ recver(void *arg)
 		TAILQ_UNLOCK(&free_tqh);
 		syslog(LOG_ERR, "get a free block success");
 	
-	/* tell the peer where to write */
+	/* tell the peer where to write
 	iperf_format_send(cb, bufblk->rdma_buf, bufblk->rdma_mr);
 	cb->send_buf.mode = kRdmaTrans_ActWrte;
 	cb->send_buf.stat = ACTIVE_WRITE_RESP;
@@ -938,25 +938,24 @@ recver(void *arg)
 		syslog(LOG_ERR, "ibv_post_send: %m");
 		return -1;
 	}
-	syslog(LOG_ERR, "ibv_post_send success");
+	syslog(LOG_ERR, "ibv_post_send success"); */
 	
-	/* wait the finish of rdma write */
+	/* wait the finish of rdma write
 	syslog(LOG_ERR, "before sem_wait(&cb->sem) 2");
 	sem_wait(&cb->sem);
-	syslog(LOG_ERR, "after sem_wait(&cb->sem) 2");
+	syslog(LOG_ERR, "after sem_wait(&cb->sem) 2"); */
 	
-	/* notify the client to go on */
+	/* notify the client to go on
 	cb->send_buf.mode = kRdmaTrans_ActWrte;
 	cb->send_buf.stat = ACTIVE_WRITE_FIN;
 	ret = ibv_post_send(cb->qp, &cb->sq_wr, &bad_wr);
 	if (ret) {
 		syslog(LOG_ERR, "ibv_post_send: %m");
 		return -1;
-	}
-		/* recv data
-		recv_dat_blk(bufblk, cb); */
-		
-		syslog(LOG_ERR, "recv data success");
+	} */
+		/* recv data */
+		recv_dat_blk(bufblk, cb);
+
 		/* insert into writer list */
 		TAILQ_LOCK(&writer_tqh);
 		TAILQ_INSERT_TAIL(&writer_tqh, bufblk, entries);
@@ -1164,28 +1163,22 @@ recv_dat_blk(BUFDATBLK *bufblk, struct rdma_cb *cb)
 	struct ibv_send_wr *bad_wr;
 	
 	/* wait for the client send ADV - READ? WRITE? */
-	syslog(LOG_ERR, "before sem_wait(&cb->sem) 1");
 	sem_wait(&cb->sem);
-	syslog(LOG_ERR, "after sem_wait(&cb->sem) 1");
 	
 	/* tell the peer where to write */
 	iperf_format_send(cb, bufblk->rdma_buf, bufblk->rdma_mr);
 	cb->send_buf.mode = kRdmaTrans_ActWrte;
 	cb->send_buf.stat = ACTIVE_WRITE_RESP;
 	tsf_setup_wr(bufblk);
-	syslog(LOG_ERR, "tsf_setup_wr success");
 	
 	ret = ibv_post_send(cb->qp, &cb->sq_wr, &bad_wr);
 	if (ret) {
 		syslog(LOG_ERR, "ibv_post_send: %m");
 		return -1;
 	}
-	syslog(LOG_ERR, "ibv_post_send success");
 	
 	/* wait the finish of rdma write */
-	syslog(LOG_ERR, "before sem_wait(&cb->sem) 2");
 	sem_wait(&cb->sem);
-	syslog(LOG_ERR, "after sem_wait(&cb->sem) 2");
 	
 	/* notify the client to go on */
 	cb->send_buf.mode = kRdmaTrans_ActWrte;
