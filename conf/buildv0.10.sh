3bdf55508e195c286ca5444d0fd4f999facd3d60
Ren Yufei <renyufei83@yahoo.com.cn>
2011-01-26 14:52:02 -0500

    rpmbuild script

--- conf/buildv0.10.sh
+++ conf/buildv0.10.sh
@@ -1,9 +1,27 @@
 #! /bin/sh
 
+GITSRCDIR=$HOME/git/rftp
+RPMDIR=$HOME/rftp
+
+# clear
+rm -rf $RPMDIR/BUILD/*
+
+# cp configuration file
+cp $GITSRCDIR/conf/rftp.spec $RPMDIR/SPECS/
+cp $GITSRCDIR/conf/rftpd.spec $RPMDIR/SPECS
+
 # client: git archive
-cd $HOME/git/rftp/netkit-ftp-0.17
-git archive --prefix=rftp-0.10/ v0.10 | gzip -9 > ../rftp-0.10.tar.gz
+cd $GITSRCDIR/netkit-ftp-0.17
+git archive --format=tar --prefix=rftp-0.10/ v0.10 | gzip -9 > $RPMDIR/SOURCES/rftp-0.10.tar.gz
+
+# build client
+cd $RPMDIR/SPECS/
+rpmbuild -ba rftp.spec
 
 # server: git archive
-cd $HOME/git/rftp/linux-ftpd-0.17
-git archive --format=tar --prefix=rftpd-0.10/ HEAD | gzip -9 > ../rftpd-0.10.tar.gz
+cd $GITSRCDIR/linux-ftpd-0.17
+git archive --format=tar --prefix=rftpd-0.10/ HEAD | gzip -9 > $RPMDIR/SOURCES/rftpd-0.10.tar.gz
+
+# build server
+cd $RPMDIR/SPECS/
+rpmbuild -ba rftpd.spec
