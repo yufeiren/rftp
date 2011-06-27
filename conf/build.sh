2e3fea8018c0bb0d7152dfda127eeb8a8d48eb80
Yufei Ren <renyufei83@gmail.com>
2011-06-27 16:05:43 -0400

    build v0.13-2

--- conf/build.sh
+++ conf/build.sh
@@ -17,7 +17,7 @@ cp $GITSRCDIR/conf/rftpd.spec $RPMDIR/SPECS/
 # client: git archive
 cd $GITSRCDIR/netkit-ftp
 # HEAD or vx.xx
-git archive --format=tar --prefix=rcftp-$VERSION-$RELEASE/ $TAG | gzip -9 > $RPMDIR/SOURCES/rcftp-$VERSION-$RELEASE.tar.gz
+git archive --format=tar --prefix=rcftp-$VERSION/ $TAG | gzip -9 > $RPMDIR/SOURCES/rcftp-$VERSION-$RELEASE.tar.gz
 
 
 # build client
@@ -26,7 +26,7 @@ rpmbuild -ba rftp.spec
 
 # server: git archive
 cd $GITSRCDIR/linux-ftpd
-git archive --format=tar --prefix=rftpd-$VERSION-$RELEASE/ $TAG | gzip -9 > $RPMDIR/SOURCES/rftpd-$VERSION-$RELEASE.tar.gz
+git archive --format=tar --prefix=rftpd-$VERSION/ $TAG | gzip -9 > $RPMDIR/SOURCES/rftpd-$VERSION-$RELEASE.tar.gz
 
 # build server
 cd $RPMDIR/SPECS/
